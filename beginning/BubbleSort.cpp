760bbeb30f59eff7a8be6080191b192b049a0b5d
Ignacio M. <igmercado46@gmail.com>
2024-06-15 18:33:40 -0300

    Adds bubble sort

---
+++ beginning/BubbleSort.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    int array[] = {1, 2, 30, 13, 2, 78, 5, 100, 78};
+    for (int numero : array){
+        std::cout << numero << '\n';
+    }
+
+    int temp;
+
+    for (int i = 0; i < sizeof(array)/sizeof(array[0]) - 1; i++) {
+        for (int j = 0; j < sizeof(array)/sizeof(array[0]) - i - 1 ; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+
+    std::cout << "************" << '\n';
+
+    for (int numero : array){
+            std::cout << numero << '\n';
+        }
+    
+}
